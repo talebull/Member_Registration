3bc112b3f74800cd86960896ffb81a624475e480
talebull <hossain.islam106@gmail.com>
2026-04-13 18:34:37 +0200

    script.js

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 const scriptURL = "PASTE_YOUR_GOOGLE_SCRIPT_URL";
 
 const form = document.getElementById("form");
-const btn = document.getElementById("submitBtn");
+const btn = document.getElementById("btn");
 const msg = document.getElementById("msg");
 
 form.addEventListener("submit", e => {
-  e.preventDefault();
+e.preventDefault();
 
-  btn.disabled = true;
-  btn.innerText = "Submitting...";
+btn.disabled = true;
+btn.innerText = "Submitting...";
 
-  const formData = new FormData(form);
-
-  fetch(scriptURL, {
-    method: "POST",
-    body: formData
-  })
-  .then(res => res.text())
-  .then(data => {
-    msg.innerHTML = "✅ Registration Successful!";
-    form.reset();
-  })
-  .catch(err => {
-    msg.innerHTML = "❌ Error! Try again.";
-  })
-  .finally(() => {
-    btn.disabled = false;
-    btn.innerText = "Register";
-  });
+fetch(scriptURL, {
+method: "POST",
+body: new FormData(form)
+})
+.then(res => res.text())
+.then(data => {
+msg.innerHTML = "✅ Registration successful!";
+form.reset();
+})
+.catch(err => {
+msg.innerHTML = "❌ Something went wrong!";
+})
+.finally(() => {
+btn.disabled = false;
+btn.innerText = "Submit Registration";
+});
 });
